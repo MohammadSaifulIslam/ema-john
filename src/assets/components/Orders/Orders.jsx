f6f9ddf2c7a40852a3daace97b5ea11cfa3ef6f1
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-04-05 12:58:22 +0600

    handle remove review item from ui and localstore function added

--- src/assets/components/Orders/Orders.jsx
+++ src/assets/components/Orders/Orders.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData()
+    const savedCart = useLoaderData();
+    const [cart,setCart] = useState(savedCart);
+    // handle remove review item
+    const handleRemoveItem = id =>{
+        const remaing = cart.filter(product => product.id !== id);
+        setCart(remaing);
+        removeFromDb(id);
+    }
     return (
         <div className='shop-container grid grid-cols-5'>
         <div className="product-contaier col-span-3 m-8 flex justify-center items-center">
@@ -13,13 +21,13 @@ const Orders = () => {
                 cart.map(product => <ReviewItem
                 key={product.id}
                 product={product}
+                handleRemoveItem={handleRemoveItem}
                 >
-
                 </ReviewItem>)
             }
            </div>
         </div>
-        <div className="cart-container col-span-2 bg-neutral sticky top-0 px-4 py-7">
+        <div className="cart-container col-span-2 bg-neutral h-screen px-4 py-7">
           <Cart cart={cart}></Cart>
         </div>
         
--- src/assets/components/ReviewItem/ReviewItem.jsx
+++ src/assets/components/ReviewItem/ReviewItem.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product,handleRemoveItem }) => {
     const { id, name, img, price, quantity, shipping } = product
-    console.log(product)
+
+
     return (
-        <div className='h-[120px] w-[570px] border border-gray rounded-lg p-2 flex gap-4 mb-5'>
+        <div className='h-[120px] w-[570px] border border-gray rounded-lg py-2 pl-2 pr-5 flex gap-4 mb-5 items-center'>
             <img className='h-full rounded-md' src={img} alt="" />
-            <div>
-                <div>
-                    <h3 className='text-xl tracking-tighter mt-1'>{name}</h3>
-                    <p>Price: ${price}</p>
-                    <p>Quantity: {quantity}</p>
-                    <p>Shipping Charge:${shipping}</p>
-                </div>
+            <div className='grow'>
+                <h3 className='text-xl tracking-tighter mt-1'>{name}</h3>
+                <p>Price: <span className='text-secondary'>${price}</span></p>
+                <p>Quantity: <span className='text-secondary'>{quantity}</span></p>
+                <p>Shipping Charge: <span className='text-secondary'>${shipping}</span></p>
             </div>
+            <button onClick={()=> handleRemoveItem(id)} className='w-14 h-14 rounded-full bg-red-300'> <FontAwesomeIcon className='text-error' icon={faTrashAlt} /></button>
         </div>
     );
 };
